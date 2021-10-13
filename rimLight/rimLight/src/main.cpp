a4c0ae591e35d2a0c229cb19278951ba547ade00
zydiii <15826138027@163.com>
2021-10-13 11:02:17 +0800

    rimLight

---
+++ rimLight/rimLight/src/main.cpp
@@ -0,0 +1,14 @@
+#include "ofMain.h"
+#include "ofApp.h"
+
+//========================================================================
+int main( ){
+	ofGLWindowSettings glSettings;
+	glSettings.setSize(1024, 728);
+	glSettings.windowMode = OF_WINDOW;
+	glSettings.setGLVersion(4, 1);
+	ofCreateWindow(glSettings);
+
+	printf("%s\n", glGetString(GL_VERSION));
+	ofRunApp(new ofApp());
+}
